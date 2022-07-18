37470b4bf7e898397f5287bdf925ce34e85992f7
unknown <krishnavizster@gmail.com>
2022-07-18 09:28:30 -0700

    'setupfile'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from tkinter import PROJECTING
+from setuptools import setup
+
+import storessales
+
+
+PROJECT_NAME='stores-sales-predictor'
+VERSION='0.0.1'
+AUTHOR='Krishna Kumar'
+DESCRIPTION='this is the store sales predector project for shoping malls'
+
+setup()
+name=PROJECT_NAME
+verssion=VERSION
+author=AUTHOR
+description=DESCRIPTION
+packages=['storessales']
