56784eb2f66dfca24bb5a22353b5617b5a3828c1
unknown <krishnavizster@gmail.com>
2022-07-18 01:26:04 -0700

    docker main yaml file created

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "store sales predection"
+    return "CI CD pipeline has been established for store sales predection"
 
 if __name__ == '__main__':
     app.run(debug=True)
